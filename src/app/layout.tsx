35503c6971de0333fcb32c9cd6e1d5c4bdf563e8
דניאל מלכה <dnylmlkh@MacBook-Pro-sl-dnyl.local>
2026-03-08 11:12:16 +0200

    Add Microsoft Clarity heatmap tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,6 +75,19 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* Microsoft Clarity */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "vsh9ttnm8v");
+            `,
+          }}
+        />
       </head>
       <body className={`${cormorant.variable} ${dmSans.variable} font-sans bg-[#0D0D1F] text-white`}>
         {children}
